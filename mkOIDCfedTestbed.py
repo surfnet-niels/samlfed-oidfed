2c3a05e1b8fd8d13b2d55dea68fa1e9760d5dab0
Niels van Dijk <niels.vandijk@surf.nl>
2025-05-20 09:35:18 +0200

    refactoring config writer - wip

--- mkOIDCfedTestbed.py
+++ mkOIDCfedTestbed.py
@@ -24,7 +24,7 @@ WRITETOLOG = False
 # Config and logs handling functions
 #
 ##################################################################################################################################
-def loadJSONconfig(json_file):
+def loadJSON(json_file):
    with open(json_file) as json_file:
      return json.load(json_file)
 
@@ -110,6 +110,191 @@ def parseFeds(fedsJson,fedsInUse):
          p(fedID + " skipped as not in use due to config")
    return RAs 
 
+class Config:
+    def __init__(self):
+        self.server_port = None
+        self.entity_id = None
+        self.authority_hints = []
+        self.signing_key_file = None
+        self.organization_name = None
+        self.data_location = None
+        self.human_readable_storage = False
+        self.metadata_policy_file = None
+        self.endpoints = {}
+        self.trust_mark_specs = []
+        self.trust_mark_owners = {}
+
+    def from_yaml(file_path):
+        config = Config()
+        with open(file_path, 'r') as f:
+            data = yaml.safe_load(f)
+            if 'server_port' in data:
+                config.server_port = data['server_port']
+            if 'entity_id' in data:
+                config.entity_id = data['entity_id']
+            if 'authority_hints' in data:
+                config.authority_hints = data['authority_hints']
+            if 'signing_key_file' in data:
+                config.signing_key_file = data['signing_key_file']
+            if 'organization_name' in data:
+                config.organization_name = data['organization_name']
+            if 'data_location' in data:
+                config.data_location = data['data_location']
+            if 'human_readable_storage' in data and data['human_readable_storage'].lower() == 'true':
+                config.human_readable_storage = True
+            if 'metadata_policy_file' in data:
+                config.metadata_policy_file = data['metadata_policy_file']
+            if 'endpoints' in data:
+                for endpoint_name, endpoint_data in data['endpoints'].items():
+                    config.endpoints[endpoint_name] = {'path': endpoint_data['path']}
+            if 'trust_mark_specs' in data:
+                for trust_mark_spec in data['trust_mark_specs']:
+                    spec = {}
+                    if 'trust_mark_id' in trust_mark_spec:
+                        spec['trust_mark_id'] = trust_mark_spec['trust_mark_id']
+                    if 'lifetime' in trust_mark_spec:
+                        spec['lifetime'] = trust_mark_spec['lifetime']
+                    if 'ref' in trust_mark_spec:
+                        spec['ref'] = trust_mark_spec['ref']
+                    if 'delegation_jwt' in trust_mark_spec:
+                        spec['delegation_jwt'] = trust_mark_spec['delegation_jwt']
+                    if 'checker' in trust_mark_spec:
+                        spec['checker'] = {'type': trust_mark_spec['checker']['type']}
+                    config.trust_mark_specs.append(spec)
+            if 'trust_mark_owners' in data:
+                for trust_mark_id, owner_data in data['trust_mark_owners'].items():
+                    owner = {}
+                    if 'entity_id' in owner_data:
+                        owner['entity_id'] = owner_data['entity_id']
+                    if 'jwks' in owner_data:
+                        owner['jwks'] = owner_data['jwks']
+                    config.trust_mark_owners[trust_mark_id] = owner
+            if 'trust_marks' in data:
+                for trust_mark in data['trust_marks']:
+                    if 'trust_mark_id' in trust_mark and 'trust_mark_issuer' in trust_mark:
+                        config.trust_marks.append(trust_mark)
+        return config
+
+    def to_yaml(self, file_path):
+        with open(file_path, 'w') as f:
+            yaml.dump(**self.__dict__), f)
+
+    def set_server_port(self, port):
+        self.server_port = port
+
+    def get_server_port(self):
+        return self.server_port
+
+    def set_entity_id(self, entity_id):
+        self.entity_id = entity_id
+
+    def get_entity_id(self):
+        return self.entity_id
+
+    def add_authority_hint(self, authority_hint):
+        if 'authority_hints' not in self.__dict__:
+            self.authority_hints = []
+        self.authority_hints.append(authority_hint)
+
+    def get_authority_hints(self):
+        return self.authority_hints
+
+   def set_server_port(self, port):
+        self.server_port = port
+
+    def get_server_port(self):
+        return self.server_port
+
+    def set_entity_id(self, entity_id):
+        self.entity_id = entity_id
+
+    def get_entity_id(self):
+        return self.entity_id
+
+    def add_authority_hint(self, authority_hint):
+        if 'authority_hints' not in self.__dict__:
+            self.authority_hints = []
+        self.authority_hints.append(authority_hint)
+
+    def get_authority_hints(self):
+        return self.authority_hints
+
+    def set_signing_key_file(self, file_path):
+        self.signing_key_file = file_path
+
+    def get_signing_key_file(self):
+        return self.signing_key_file
+
+    def set_organization_name(self, name):
+        self.organization_name = name
+
+    def get_organization_name(self):
+        return self.organization_name
+
+    def set_data_location(self, location):
+        self.data_location = location
+
+    def get_data_location(self):
+        return self.data_location
+
+    def set_human_readable_storage(self, storage):
+        if storage.lower() == 'true':
+            self.human_readable_storage = True
+        else:
+            self.human_readable_storage = False
+
+    def get_human_readable_storage(self):
+        return self.human_readable_storage
+
+    def set_metadata_policy_file(self, file_path):
+        self.metadata_policy_file = file_path
+
+    def get_metadata_policy_file(self):
+        return self.metadata_policy_file
+
+    def add_endpoint(self, name, path):
+        if 'endpoints' not in self.__dict__:
+            self.endpoints = {}
+        self.endpoints[name] = {'path': path}
+
+    def get_endpoints(self):
+        return self.endpoints
+
+    def set_trust_mark_specs(self, specs):
+        self.trust_mark_specs = specs
+
+    def get_trust_mark_specs(self):
+        return self.trust_mark_specs
+
+    def add_trust_mark_spec(self, spec):
+        if 'trust_mark_specs' not in self.__dict__:
+            self.trust_mark_specs = []
+        self.trust_mark_specs.append(spec)
+
+    def set_trust_marks(self, marks):
+        self.trust_marks = marks
+
+    def get_trust_marks(self):
+        return self.trust_marks
+
+    def add_trust_mark_owner(self, trust_mark_id, entity_id, jwks):
+        if 'trust_mark_owners' not in self.__dict__:
+            self.trust_mark_owners = {}
+        self.trust_mark_owners[trust_mark_id] = {'entity_id': entity_id, 'jwks': jwks}
+
+    def get_trust_mark_owners(self):
+        return self.trust_mark_owners
+
+
+
+# Example usage
+config = Config.from_yaml('path/to/config.yaml')
+print(config.server_port)  # prints the server port from the YAML file
+config.to_yaml('new/path/new_config.yaml')  # writes the config to a new file
+
+
+
+
 def main(argv):
 
    ROOTPATH='/home/debian/samlfed-oidfed'
@@ -134,7 +319,7 @@ def main(argv):
       allFeds = getFeds(edugainFedsURL, INPUT_PATH)
       raConf = parseFeds(allFeds, ['IDEM', 'SURFCONEXT', 'HAKA'])
    else:
-      raConf = loadJSONconfig(CONFIG_PATH + 'RAs.json')
+      raConf = loadJSON(CONFIG_PATH + 'RAs.json')
 
    # Add eduGAIN as a TA
    raConf['edugain'] = {
@@ -187,7 +372,8 @@ def main(argv):
       "refeds": {
          "name": "REFEDs Trustmark Owner",
          "url": "https://refeds." + TESTBED_BASEURL,
-         "tas": ["edugain"]
+         "tas": ["edugain"],
+         "jwks": ""
       }, 
 
    }
@@ -278,8 +464,28 @@ def main(argv):
          write_file(result, TESTBED_PATH+'/' +tmo+ '/data/tm-delegation.yaml', mkpath=False, overwrite=True)
 
       # Now run the TMO docker container to generate delegation jwt on the fly
-      # generte JWKS
+      try:
+         os.popen('docker run --rm --user "${UID}":"${GID}" -v "'+ TESTBED_PATH+'/' +tmo+'/data:/refeds" myoidc/oidfed-gota /tacli delegation --json /'+tmo+'/tm-delegation.yaml')
+      except:
+         p("Could not create delegation JWT for TMO " + tmo) 
+      # generate JWKS
+      try:
+         tmoDel = loadJSON(TESTBED_PATH+'/' +tmo+'/data/tm-delegation.json')
+      except:
+         p("Could not parse delegation data for TMO " + tmo + ' in file ' + TESTBED_PATH+'/' +tmo+'/tm-delegation.json') 
+
+      #pj(tmoDel)
+      tmoConf[tmo]['jwks'] = tmoDel['jwks']
+      tmoConf[tmo]['trust_mark_issuers'] = {}
+      # loop over TMs for this TMO
+      for tm in tmoDel['trust_marks']:
+         p(tm["trust_mark_id"])
+         #p(tm["trust_mark_issuers"])
+         for tmi in tm["trust_mark_issuers"]:
+            tmoConf[tmo]['trust_mark_issuers'][tmi['entity_id']] = tmi['delegation_jwt']
+
       # Put JWKS and delegation JWT in config so we can add it to the TA config
+      pj(tmoConf)
 
 
    # Add Trust Mark Issuers
@@ -301,9 +507,12 @@ def main(argv):
    #
    for ra in raConf.keys():
       if ra == 'edugain':
-         # TODO: for edugain replace with propper YAML handling
+         # TODO: for edugain replace with proper YAML handling
+         # TODO: proper TMI handling
          conf = {
-            'testbed_domain': 'oidfed.lab.surf.nl'
+            'testbed_domain': 'oidfed.lab.surf.nl',
+            'refeds_tmo_url': 'https://refeds.oidfed.lab.surf.nl',
+            'refeds_jwks': tmoConf['refeds']['jwks']
          }
          
          with open('templates/edugain_config.yaml', 'r') as f:
@@ -314,14 +523,16 @@ def main(argv):
          os.popen('cp templates/edugain_metadata-policy.json '+TESTBED_PATH+'/' +ra+ '/data/metadata-policy.json') 
 
       else:
+         # TODO: proper TMI handling
          conf = {
             'entity_id': raConf[ra]['ta_url'],
             'ta': 'https://edugain.oidfed.lab.surf.nl',
             'orgname': raConf[ra]['name'],
             'refeds_tmo_url': 'https://refeds.oidfed.lab.surf.nl',
+            'refeds_jwks': tmoConf['refeds']['jwks'],
             'edugain_member_tmi_url': 'https://edugain.oidfed.lab.surf.nl',
             'refeds_sirtfi_tmi_url': raConf[ra]['ta_url'],
-            
+            'refeds_delegation_jwt': tmoConf['refeds']['trust_mark_issuers'][raConf[ra]['ta_url']]
          }
 
          with open('templates/ta_config.yaml', 'r') as f:
