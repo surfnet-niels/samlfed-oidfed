4d569d8043e418b2e94da5bdac4f2c3e734a5940
Niels van Dijk <niels.vandijk@surf.nl>
2025-07-08 11:31:06 +0200

    Fixed issue in diplay URLs

--- mkOIDfedEntityConfig.py
+++ mkOIDfedEntityConfig.py
@@ -915,7 +915,7 @@ def main(argv):
       #Export and Write private key
       writeFile(exportKey(leafKeys, "private"), leafID, OUTPUT_PATH, "jwk")
       writeFile(leafMeta, leafID, OUTPUT_PATH, "json")
-      htmlContent = "<h3>" + leafEntityID + "</h3><p>OIDFed Entity Configuration:<ul>" + "<li><a href='"+ entityList[leafID]['metadata']['sub'] + "/.well-known/openid-federation'>JWT</a> (Will be downloaded)</li>" + "<li><a href='"+ entityList[leafID]['metadata']['sub'] + "/entity.json' target='_blank'>JSON</a> (Opens in new window)</li>" + "</ul></p>"
+      htmlContent = "<h3>" + leafEntityID + "</h3><p>OIDFed Entity Configuration:<ul>" + "<li><a href='"+ entityList[leafID]['metadata']['sub'] + ".well-known/openid-federation'>JWT</a> (Will be downloaded)</li>" + "<li><a href='"+ entityList[leafID]['metadata']['sub'] + "entity.json' target='_blank'>JSON</a> (Opens in new window)</li>" + "</ul></p>"
       writeFile(htmlContent, leafID, OUTPUT_PATH, "html")
 
       #Generate and Write jwt signed metadata
